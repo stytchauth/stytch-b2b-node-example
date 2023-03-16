6921a32e3e1fc4e29aee00197ccdf3b3abec9785
Maxwell Gerber <max@stytch.com>
2023-03-16 15:42:50 -0700

    checkin: disco client

--- lib/loadStytch.ts
+++ lib/loadStytch.ts
@@ -1,6 +1,7 @@
 import * as stytch from "stytch";
+import {Discovery} from "./disco";
 
-let client: stytch.B2BClient;
+let client: CustomStytchB2BClient;
 
 export const publicToken = process.env.NEXT_PUBLIC_STYTCH_PUBLIC_TOKEN;
 
@@ -26,9 +27,21 @@ export const formatSSOStartURL = (connection_id: string): string => {
   return `${stytchEnv}public/sso/start?connection_id=${connection_id}&public_token=${publicToken}`;
 };
 
+export class CustomStytchB2BClient extends stytch.B2BClient {
+  discovery: Discovery;
+  constructor(...args: ConstructorParameters<typeof stytch.B2BClient>) {
+    super(...args);
+
+    // fetchConfig is private, but that won't stop us!!!!!!!
+    // @ts-ignore
+    this.discovery = new Discovery(this.fetchConfig);
+  }
+}
+
+
 const loadStytch = () => {
   if (!client) {
-    client = new stytch.B2BClient({
+    client = new CustomStytchB2BClient({
       project_id: process.env.STYTCH_PROJECT_ID || "",
       secret: process.env.STYTCH_SECRET || "",
       env: stytchEnv,
